ed44086f40283e2e77bb36d2ff835b8a0f12603b
Dumitru <dima50066@gmail.com>
2024-06-05 14:26:06 +0200

    rework pixabay-js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,14 @@
 const API_KEY = '44238533-8b617bda18ddca61b92d6b256';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query) {
   const response = await fetch(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
+    `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
       query
     )}&image_type=photo&orientation=horizontal&safesearch=true`
   );
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
-  const data = await response.json();
-  return data.hits;
+  return await response.json();
 }
